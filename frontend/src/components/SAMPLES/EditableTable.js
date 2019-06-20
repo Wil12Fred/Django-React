a2f61c1cf9afea39c1c6d58df7a4b4f2b2a3b506
root <wilbert_030@icloud.com>
2019-06-20 14:38:22 -0500

    deleting trash

--- frontend/src/components/SAMPLES/EditableTable.js
+++ frontend/src/components/SAMPLES/EditableTable.js
@@ -9,43 +9,6 @@ class Products extends React.Component {
     this.state = {};
     this.state.filterText = "";
     this.state.products = [
-      {
-        id: 1,
-        category: 'Sporting Goods',
-        price: '49.99',
-        qty: 12,
-        name: 'football'
-      }, {
-        id: 2,
-        category: 'Sporting Goods',
-        price: '9.99',
-        qty: 15,
-        name: 'baseball'
-      }, {
-        id: 3,
-        category: 'Sporting Goods',
-        price: '29.99',
-        qty: 14,
-        name: 'basketball'
-      }, {
-        id: 4,
-        category: 'Electronics',
-        price: '99.99',
-        qty: 34,
-        name: 'iPod Touch'
-      }, {
-        id: 5,
-        category: 'Electronics',
-        price: '399.99',
-        qty: 12,
-        name: 'iPhone 5'
-      }, {
-        id: 6,
-        category: 'Electronics',
-        price: '199.99',
-        qty: 23,
-        name: 'nexus 7'
-      }
     ];
 
   }
